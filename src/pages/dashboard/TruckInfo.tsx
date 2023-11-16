50032976aa5691e39905714d5279952e1155d04d
tatendabakozw <tatendabakozw@gmail.com>
2023-11-16 15:46:03 +0200

    vercel json

--- src/pages/dashboard/TruckInfo.tsx
+++ src/pages/dashboard/TruckInfo.tsx
@@ -11,10 +11,10 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 const haulage_cycles = [
   { activity: "Spot time load", to_crusher: 1.92, to_dump: 2.0 },
-  { activity: "Loaaad time", to_crusher: 2.23, to_dump: 2.3 },
+  { activity: "Load time", to_crusher: 2.23, to_dump: 2.3 },
   { activity: "Full Haul", to_crusher: 8.04, to_dump: 7.9 },
   { activity: "Spot time dump", to_crusher: 0.81, to_dump: 0.9 },
-  { activity: "Spot time load", to_crusher: 1.97, to_dump: 2.0 },
+  { activity: "Spot time haul", to_crusher: 1.97, to_dump: 2.0 },
   { activity: "Spot time load", to_crusher: 3.82, to_dump: 5.5 },
 ];
 
