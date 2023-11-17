2edcc9b5bf5bcbcb660f12d6b74220657c715ea7
tatendabakozw <tatendabakozw@gmail.com>
2023-11-17 09:26:18 +0200

    feature: added totaal times

--- src/pages/dashboard/TruckInfo.tsx
+++ src/pages/dashboard/TruckInfo.tsx
@@ -1,21 +1,19 @@
-import BarChart from "@components/bar-chat/BarChart";
 import HaulageProfiles from "@components/haulage-profiles/HaulageProfiles";
-import DashboardLayout from "@layouts/DashboardLayout";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import DashboardLayout from "@layouts/DashboardLayout";
+import BarChart from "@components/bar-chat/BarChart";
 import { Doughnut } from "react-chartjs-2";
 import Map from "react-map-gl";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
-
 const haulage_cycles = [
-  { activity: "Spot time load", to_crusher: 1.92, to_dump: 2.0 },
-  { activity: "Load time", to_crusher: 2.23, to_dump: 2.3 },
-  { activity: "Full Haul", to_crusher: 8.04, to_dump: 7.9 },
+  { activity: "Spot time load", to_crusher: 1.92, to_dump: 2.0, },
   { activity: "Spot time dump", to_crusher: 0.81, to_dump: 0.9 },
   { activity: "Spot time haul", to_crusher: 1.97, to_dump: 2.0 },
   { activity: "Spot time load", to_crusher: 3.82, to_dump: 5.5 },
+  { activity: "Load time", to_crusher: 2.23, to_dump: 2.3 },
+  { activity: "Full Haul", to_crusher: 8.04, to_dump: 7.9 },
 ];
 
 export const data = {
@@ -84,7 +82,9 @@ const TruckInfo = () => {
                 <Doughnut data={data1} />
               </div>
               <div className="flex flex-col col-span-2 bg-white p-4 rounded-xl">
-                <p className="pb-4 text-slate-900 font-semibold text-sm">Average Haulage Cycle Time</p>
+                <p className="pb-4 text-slate-900 font-semibold text-sm">
+                  Average Haulage Cycle Time
+                </p>
                 <div className="grid grid-cols-4 border-b border-slate-100 text-slate-900 font-semibold pb-2 mb-2">
                   <div className="col-span-2">Activity</div>
                   <div className="col-span-1">To Crusher</div>
@@ -100,6 +100,11 @@ const TruckInfo = () => {
                     <div className="col-span-1">{item.to_dump}</div>
                   </div>
                 ))}
+                <div className="grid grid-cols-4 text-slate-900 font-medium">
+                  <div className="col-span-2">total cycle time</div>
+                  <div className="col-span-1">{haulage_cycles.reduce((n, {to_crusher}) => n + to_crusher, 0)}</div>
+                    <div className="col-span-1">{haulage_cycles.reduce((n, {to_dump}) => n + to_dump, 0)}</div>
+                </div>
               </div>
               <div className="col-span-2  bg-white p-4 rounded-xl flex flex-col">
                 <BarChart />
